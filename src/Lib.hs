c8e26bb05ceab0311ec5836d0556503b0a7bec99
straszydlo <kamil.szydlo.ks@gmail.com>
2022-12-03 23:00:43 +0100

    Day 1, pt 2

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,36 +1,36 @@
-module Lib
-    ( someFunc
-    ) where
+module Lib where
 
 import Control.Monad.State
+import Data.List(sort)
 import Data.Maybe
 import Text.Read(readMaybe)
 
-type Result = Int
+newtype Result = Result { highest :: [Int] } deriving Show
 type Accumulation = Int
 
-data Step = UpdateMax | Accumulate Int
+data Step = UpdateMax | Accumulate Int deriving Show
 
 parseStep :: String -> Step
 parseStep str = maybe UpdateMax Accumulate $ readMaybe str
 
 solve :: [Step] -> Result
 solve steps =
-  let (acc, res) = runState (solveS steps) 0
-  in  max acc res
+  let (acc, res) = runState (solveS update steps) (Result [])
+  in  update acc res
+  where update acc result = Result . take 3 . reverse . sort $ acc : (highest result)
 
-solveS :: [Step] -> State Result Accumulation
-solveS steps = foldM accumulateOrUpdate 0 steps
+solveS :: (Accumulation -> Result -> Result) -> [Step] -> State Result Accumulation
+solveS updater steps = foldM (accumulateOrUpdate updater) 0 steps
 
-accumulateOrUpdate :: Accumulation -> Step -> State Result Accumulation
-accumulateOrUpdate acc step = case step of
+accumulateOrUpdate :: (Accumulation -> Result -> Result) -> Accumulation -> Step -> State Result Accumulation
+accumulateOrUpdate updater acc step = case step of
   UpdateMax -> do
-    modify $ max acc
+    modify $ updater acc
     return 0
   Accumulate value ->
     return $ acc + value
 
 someFunc :: String -> IO ()
 someFunc filename =
-  readFile filename >>= print . solve . map parseStep . lines
+  readFile filename >>= print . sum . highest . solve . map parseStep . lines
 
