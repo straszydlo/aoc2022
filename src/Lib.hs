1e76bc2a6df05e33818a569fe34b7115dd977e3f
straszydlo <kamil.szydlo.ks@gmail.com>
2022-12-03 18:17:39 +0100

    Day 1, pt 1

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,5 +2,35 @@ module Lib
     ( someFunc
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import Control.Monad.State
+import Data.Maybe
+import Text.Read(readMaybe)
+
+type Result = Int
+type Accumulation = Int
+
+data Step = UpdateMax | Accumulate Int
+
+parseStep :: String -> Step
+parseStep str = maybe UpdateMax Accumulate $ readMaybe str
+
+solve :: [Step] -> Result
+solve steps =
+  let (acc, res) = runState (solveS steps) 0
+  in  max acc res
+
+solveS :: [Step] -> State Result Accumulation
+solveS steps = foldM accumulateOrUpdate 0 steps
+
+accumulateOrUpdate :: Accumulation -> Step -> State Result Accumulation
+accumulateOrUpdate acc step = case step of
+  UpdateMax -> do
+    modify $ max acc
+    return 0
+  Accumulate value ->
+    return $ acc + value
+
+someFunc :: String -> IO ()
+someFunc filename =
+  readFile filename >>= print . solve . map parseStep . lines
+
