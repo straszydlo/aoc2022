06ce3b2180ecaf434de2a4c869c446c8770ca67c
straszydlo <kamil.szydlo.ks@gmail.com>
2022-12-04 22:12:03 +0100

    Day 3, pt 2

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,6 +2,7 @@ module Lib
     ( someFunc
     ) where
 
+import Control.Monad.State
 import Data.Map(Map, (!))
 import qualified Data.Map as Map
 import Data.Set(Set)
@@ -9,23 +10,16 @@ import qualified Data.Set as Set
 
 type Item = Char
 type Priority = Int
-data Rucksack = Rucksack { one :: Set Item, two :: Set Item }
+newtype Rucksack = Rucksack (Set Item)
 
-prioritize :: Rucksack -> Priority
-prioritize rucksack = priorities ! (common rucksack)
+data AccumulateUpToTwo = None | One Rucksack | Two Rucksack Rucksack
 
 rucksack :: [Item] -> Rucksack
-rucksack items
-  | even $ length items =
-      let (one, two) = splitAt ((length items) `div` 2) items
-      in Rucksack (Set.fromList one) (Set.fromList two)
-  | otherwise = error $ "uneven number of items in rucksack: " ++ items
+rucksack = Rucksack . Set.fromList
 
-common :: Rucksack -> Item
-common rucksack = case Set.null $ intersection of
-  True -> error "no common items found"
-  False -> head $ Set.toList intersection
-  where intersection = Set.intersection (one rucksack) (two rucksack)
+common :: [Rucksack] -> Item
+common [] = error "No rucksacks given"
+common ((Rucksack r):rest) = head . Set.toList $ foldl (\acc (Rucksack x) -> Set.intersection x acc) r rest 
 
 
 priorities :: Map Item Priority
@@ -33,7 +27,18 @@ priorities = Map.fromList $ lowercases ++ uppercases
   where lowercases = zip ['a'..'z'] [1..]
         uppercases = zip ['A'..'Z'] [27..]
 
+solveS :: [Rucksack] -> State Priority ()
+solveS = void . foldM accumulate None
+
+accumulate :: AccumulateUpToTwo -> Rucksack -> State Priority AccumulateUpToTwo
+accumulate None r = return $ One r
+accumulate (One r1) r2 = return $ Two r1 r2
+accumulate (Two r1 r2) r3 = modify ((+) (priorities ! (common [r1, r2, r3]))) >> return None
+
+solve :: [Rucksack] -> Priority
+solve rucksacks = execState (solveS rucksacks) 0
+
 someFunc :: String -> IO ()
 someFunc filename =
-  readFile filename >>= print . sum . map prioritize . map rucksack . lines
+  readFile filename >>= print . solve . map rucksack . lines
 
