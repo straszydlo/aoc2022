d2cf7fbc900f972b2b724eabe003da81540964ca
straszydlo <kamil.szydlo.ks@gmail.com>
2022-12-04 01:04:19 +0100

    Day 2, pt 2

--- src/Lib.hs
+++ src/Lib.hs
@@ -28,19 +28,27 @@ score = sum . map evaluateGame . map parseGame
 
 --this should handle errors gracefully, but we insanely assume sane input
 parseGame :: String -> Game
-parseGame [x, ' ', y] = Game { theirMove = parseMove x, ourMove = parseMove y }
+parseGame [x, ' ', y] = Game theirMove ourMove
+  where theirMove = parseMove x
+        ourMove = parseOurMove theirMove y 
 parseGame other = error $ "invalid input: " ++ other
 
 --this should handle errors gracefully, but we insanely assume sane input
 parseMove :: Char -> Move
 parseMove = \case
   'A' -> Rock
-  'X' -> Rock
   'B' -> Paper
-  'Y' -> Paper
   'C' -> Scissors
-  'Z' -> Scissors
   other   -> error $ "invalid move: " ++ [other]
+
+parseOurMove :: Move -> Char -> Move
+parseOurMove Rock 'X' = Scissors
+parseOurMove Rock 'Z' = Paper
+parseOurMove Paper 'X' = Rock
+parseOurMove Paper 'Z' = Scissors
+parseOurMove Scissors 'X' = Paper
+parseOurMove Scissors 'Z' = Rock
+parseOurMove move 'Y' = move
 
 evaluateGame :: Game -> Score
 evaluateGame game = points game + points (ourMove game)
